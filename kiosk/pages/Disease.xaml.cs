968c11a85c8acaa887b1800c879a6b4eac656139
linhjoo <cheonhalinh@gmail.com>
2022-04-08 12:11:30 +0900

    init

---
+++ kiosk/pages/Disease.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HelixToolkit.Wpf;
+using System.Windows.Media.Media3D;
+
+namespace kiosk.pages
+{
+    /// <summary>
+    /// Disease.xaml에 대한 상호 작용 논리
+    /// </summary>
+    /// 
+    public enum FOOT_SIDES { LEFT, RIGHT };
+    public partial class Disease : Page
+    {
+        public Disease()
+        {
+            InitializeComponent();
+
+            LoadResultData();
+
+            GetMeshFromPly(FOOT_SIDES.LEFT, @"C:\HBT_Foot_Scanner\Result\Demo_Left.ply");
+            GetMeshFromPly(FOOT_SIDES.RIGHT, @"C:\HBT_Foot_Scanner\Result\Demo_Right.ply");
+
+            // C:\HBT_Foot_Scanner\Data\insole_sample\180_L.ply
+
+        }
+
+        private void LoadResultData()
+        {
+            IniFile ini = new IniFile();
+            ini.Load(@"C:\HBT_Foot_Scanner\Result\measurements.ini");
+
+            double footLengthL = double.Parse(ini["FOOT_SIZE"]["L_foot length"].ToString().Replace(" mm", ""));
+            double footWidthL = double.Parse(ini["FOOT_SIZE"]["L_foot width"].ToString().Replace(" mm", ""));
+            double footLengthR = double.Parse(ini["FOOT_SIZE"]["R_foot length"].ToString().Replace(" mm", ""));
+            double footWidthR = double.Parse(ini["FOOT_SIZE"]["R_foot width"].ToString().Replace(" mm", ""));
+
+            FootLengthL.Text = string.Format("{0:0.0}mm", footLengthL);
+            FootWidthL.Text = string.Format("{0:0.0}mm", footWidthL);
+            FootLengthR.Text = string.Format("{0:0.0}mm", footLengthR);
+            FootWidthR.Text = string.Format("{0:0.0}mm", footWidthR);
+        }
+
+        private void GetMeshFromPly(FOOT_SIDES foot_side, string _path_model)
+        {
+            if (File.Exists(_path_model))
+            {
+                MeshGeometry3D _geometry = getGeometryFromPLY(_path_model, false);
+                if (foot_side == FOOT_SIDES.LEFT)
+                {
+                    model1.Points = _geometry.Positions;
+                    model1.Color = Colors.Beige;
+                }
+                else
+                {
+                    model2.Points = _geometry.Positions;
+                    model2.Color = Colors.Beige;
+                }
+            }
+        }
+
+        private MeshGeometry3D getGeometryFromPLY(string path_ply, bool flip)
+        {
+            MeshGeometry3D geometry = new MeshGeometry3D();
+            geometry.TriangleIndices = new Int32Collection();
+            geometry.TextureCoordinates = new PointCollection();
+            geometry.Positions = new Point3DCollection();
+
+            int idx = 0;
+            string line;
+            StreamReader reader = new StreamReader(path_ply);
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (idx++ < 10) { continue; }
+                try
+                {
+                    if (idx % 2 == 0) { continue; }
+                    string[] _str_points = line.Trim().Split(' ');
+                    geometry.Positions.Add(
+                        new Point3D(
+                            Double.Parse(_str_points[0]) * (flip ? -1 : 1),
+                            Double.Parse(_str_points[1]),
+                            Double.Parse(_str_points[2])
+                        ));
+                }
+                catch (Exception) { }
+            }
+            return geometry;
+        }
+
+        private void Cancel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (this.NavigationService.CanGoBack)
+            {
+                this.NavigationService.GoBack();
+            }
+        }
+
+        private void Confirm_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("pages/Purchase.xaml", UriKind.Relative));
+        }
+
+        private void Rescan_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.NavigationService.CanGoBack)
+            {
+                this.NavigationService.GoBack();
+            }
+        }
+    }
+}
---
+++ kiosk/pages/Scan.xaml.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Threading;
+using System.ComponentModel;
+using System.Windows.Threading;
+
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System.IO;
+using System.Drawing;
+
+namespace kiosk.pages
+{
+    /// <summary>
+    /// Scan.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Scan : Page
+    {
+        private BackgroundWorker worker;
+        private readonly VideoCapture capture;
+
+        public Scan()
+        {
+            InitializeComponent();
+
+            capture = new VideoCapture();
+
+            EnableButton(btn_Reset_Scan, true);
+            EnableButton(btn_See_Result, false);
+
+            LoadIni();
+            InitWorkerThread();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            capture.Open(0, VideoCaptureAPIs.ANY);
+            if (!capture.IsOpened())
+            {
+                return;
+            }
+            RunTask();
+        }
+
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            worker.CancelAsync();
+
+            capture.Dispose();
+        }
+
+        private void Btn_Back_Click(object sender, RoutedEventArgs e)
+        {
+            EnableButton((Button)sender, false);
+            NavigationService.Navigate(new Uri("pages/Main.xaml", UriKind.Relative));
+        }
+
+        private void Btn_Reset_Scan_Click(object sender, RoutedEventArgs e)
+        {
+            EnableButton((Button)sender, false);
+
+            try
+            {
+                ProcessStart(@"C:\HBT_Foot_Scanner\Foot_Scan\HBT_Foot_Scanning.vbs");
+                worker.RunWorkerAsync();
+                tb_scanStart.Text = "스캔 중";
+            }
+            catch
+            {
+                Console.WriteLine("실행파일이 없습니다");
+
+            }
+        }
+
+        private void Btn_close_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (this.NavigationService.CanGoBack)
+            {
+                this.NavigationService.GoBack();
+            }
+        }
+
+        private void Btn_See_Result_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("pages/ScanResult.xaml", UriKind.Relative));
+        }
+
+        private async void RunTask()
+        {
+            var task = Task.Run(() => Run_Camera());
+            await task;
+        }
+
+        private bool Run_Camera()
+        {
+            while (true)
+            {
+                try
+                {
+                    using (var frameMat = capture.RetrieveMat())
+                    {
+                        var frameBitmap = BitmapConverter.ToBitmap(frameMat);
+                        Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate { VideoViewer.Source = ConvertBmpToImageSource(frameBitmap); }));
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("예외발생");
+                }
+
+                Thread.Sleep(100);
+            }
+        }
+
+        public void InitWorkerThread()
+        {
+            worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true; //작업의 진행률이 바뀔때 ProgressChanged 이벤트 발생여부 체크
+            worker.WorkerSupportsCancellation = true; //작업 취소 가능 여부 true 로 설정
+
+            worker.DoWork += new DoWorkEventHandler(worker_DoWork); //해야할 작업을 실행할 메서드 정의
+            worker.ProgressChanged += new ProgressChangedEventHandler(worker_ProgressChanged); //UI쪽에 진행사항을 보여줌, WorkerReportsProgress 속성값이 true 일때만 이벤트 발생
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(worker_RunWorkerCompleted);//작업이 완료되었을 때 실행할 콜백메서드 정의
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            IniFile ini = new IniFile();
+            ini.Load(@"C:/HBT_Foot_Scanner/Result/status.ini");
+            string status = ini["Progress"]["status"].ToString();
+            string percent = ini["Progress"]["percent"].ToString();
+
+            while (int.Parse(percent) < 100)
+            {
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    return; //about work, if it's cancelled;
+                }
+                try
+                {
+                    ini.Load(@"C:/HBT_Foot_Scanner/Result/status.ini");
+                    status = ini["Progress"]["status"].ToString();
+                    percent = ini["Progress"]["percent"].ToString();
+
+                    worker.ReportProgress(Convert.ToInt32(percent));
+                    Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate { tb_status.Text = status; }));
+                }
+                catch
+                {
+                    Debug.WriteLine("ini파일이 없습니다.");
+                }
+
+                Thread.Sleep(100);
+            }
+        }
+
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            tb_progress.Text = e.ProgressPercentage.ToString() + "%";
+        }
+
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+            }
+            EnableButton(btn_See_Result, true);
+            EnableButton(btn_Reset_Scan, true);
+
+            tb_scanStart.Text = "다시 스캔";
+            return;
+        }
+
+        private void LoadIni()
+        {
+            IniFile ini = new IniFile();
+            try
+            {
+                ini.Load(@"C:/HBT_Foot_Scanner/Result/Status.ini");
+            }
+            catch
+            {
+                Debug.WriteLine("ini파일이 없습니다.");
+            }
+
+            string temp = ini["Progress"]["percent"].ToString();
+
+            if (int.Parse(temp) != 0)
+            {
+                ini["progress"]["percent"] = "0";
+                ini["progress"]["status"] = "준비";
+                ini.Save(@"C:/HBT_Foot_Scanner/Result/Status.ini");
+                ini.Load(@"C:/HBT_Foot_Scanner/Result/Status.ini");
+            }
+            string status = ini["Progress"]["status"].ToString();
+            string percent = ini["Progress"]["percent"].ToString();
+
+            tb_status.Text = status;
+            tb_progress.Text = percent + "%";
+        }
+
+        public BitmapImage ConvertBmpToImageSource(Bitmap src)
+        {
+            MemoryStream ms = new MemoryStream();
+            ((System.Drawing.Bitmap)src).Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
+        }
+
+        private void ProcessStart(string fileName)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo(fileName);
+            Process.Start(startInfo);
+        }
+
+        private void ProcessKill(string fileName)
+        {
+            foreach (Process process in Process.GetProcesses())
+            {
+                if (process.ProcessName.StartsWith(fileName))
+                {
+                    process.Kill();
+                }
+            }
+        }
+
+        private void EnableButton(Button btn, bool enable)
+        {
+            if(enable == true)
+            {
+                btn.IsEnabled = true;
+                btn.Opacity = 1;
+            }
+            else
+            {
+                btn.IsEnabled = false;
+                btn.Opacity = 0.3;
+            }
+        }
+    }
+}
+
